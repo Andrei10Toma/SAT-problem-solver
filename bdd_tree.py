246f27c1e71b3f9f40e8a994b0629aa5d48027e6
Andrei10Toma <andrei10toma@yahoo.com>
2020-12-21 19:38:18 +0200

    FNC and BDD done

---
+++ bdd_tree.py
@@ -0,0 +1,25 @@
+# TODO: data will save the clause list
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def set_left(self, left_node):
+        self.left = left_node
+
+    def set_right(self, right_node):
+        self.right = right_node
+
+    def set_data(self, data):
+        self.data = data
+
+    def __str__(self):
+        return str(self.data)
+
+    def preorder_traverse(self, start_node):
+        if start_node is None:
+            return
+        print(start_node.data)
+        self.preorder_traverse(start_node.left)
+        self.preorder_traverse(start_node.right)
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Toma Andrei 321CB
-# given input: (0V~1)^(~0V1)
+# given input: (11V2V~30)^(2V30V11)^(11V30)^(~2V11)
 # SAT solver
+from bdd_tree import Node
+from copy import deepcopy
+
 
 def cnf_extract_clauses(cnf_clauses):
     clauses = cnf_clauses.split(")^(")
@@ -23,7 +26,7 @@ def generate_fnc(clauses_fnc, variable_dictionary):
 
 
 def fnc_input(cnf_modified):
-    index_columns = 0
+    variable_counter = 0
     variable_dictionary = {}
     clauses = cnf_extract_clauses(cnf_modified)
     for clause in clauses:
@@ -31,12 +34,12 @@ def fnc_input(cnf_modified):
         for literal in literals:
             if literal[0] != "~":
                 if int(literal) not in variable_dictionary.keys():
-                    variable_dictionary[int(literal)] = index_columns
-                    index_columns += 1
+                    variable_dictionary[int(literal)] = variable_counter
+                    variable_counter += 1
             else:
                 if int(literal[1:]) not in variable_dictionary.keys():
-                    variable_dictionary[int(literal[1:])] = index_columns
-                    index_columns += 1
+                    variable_dictionary[int(literal[1:])] = variable_counter
+                    variable_counter += 1
     return generate_fnc(clauses, variable_dictionary)
 
 
@@ -53,7 +56,6 @@ def check_clause(clause, interpret):
 
 def test_fnc(fnc, n, index, interpretation):
     if index == n:
-        print(interpretation)
         for clause in fnc.values():
             if check_clause(clause, interpretation) is False:
                 return False
@@ -67,18 +69,140 @@ def test_fnc(fnc, n, index, interpretation):
     return False
 
 
-def create_bdd(bdd_cnf):
-    pass
+def get_number_of_variables(clauses):
+    variable_list = []
+    counter = 0
+    for clause in clauses:
+        literals = clause.split("V")
+        for literal in literals:
+            if literal[0] != "~":
+                if int(literal) not in variable_list:
+                    variable_list.append(int(literal))
+                    counter += 1
+            else:
+                if int(literal[1:]) not in variable_list:
+                    variable_list.append(int(literal[1:]))
+                    counter += 1
+    return counter, variable_list
+
+
+def create_bdd_tree(current_node, variable_list, index):
+    ok = 0
+    ok_break = -1
+    if index == len(variable_list) or current_node.data == 1 or current_node.data == 0:
+        return
+    current_clauses = deepcopy(current_node.data)
+    non_variable = "~" + str(variable_list[index])
+    variable = str(variable_list[index])
+    counter_pops = 0
+    for i in range(len(current_clauses)):
+        i = i - counter_pops
+        new_clause = ""
+        if i < len(current_clauses):
+            literals = current_clauses[i].split("V")
+            if non_variable in literals:
+                if index == len(variable_list) - 1:
+                    ok = 1
+                else:
+                    current_clauses.pop(i)
+                    if len(current_clauses) == 0:
+                        ok = 1
+                        ok_break = 1
+                        break
+                    counter_pops += 1
+            elif variable in literals:
+                if len(literals) == 1:
+                    ok = 0
+                    ok_break = 1
+                    break
+                else:
+                    for j in range(len(literals)):
+                        if variable == literals[j]:
+                            continue
+                        else:
+                            new_clause += literals[j] + "V"
+                    new_clause = new_clause[:-1]
+                    current_clauses[i] = new_clause
+    if index < len(variable_list) - 1 and ok_break == -1:
+        added_node = Node(current_clauses)
+    else:
+        added_node = Node(ok)
+    current_node.set_left(added_node)
+    create_bdd_tree(current_node.left, variable_list, index + 1)
+    ok = 0
+    ok_break = -1
+    current_clauses = deepcopy(current_node.data)
+    non_variable = "~" + str(variable_list[index])
+    variable = str(variable_list[index])
+    counter_pops = 0
+    for i in range(len(current_clauses)):
+        ok_break = -1
+        i = i - counter_pops
+        new_clause = ""
+        if i < len(current_clauses):
+            literals = current_clauses[i].split("V")
+            if variable in literals:
+                if index == len(variable_list) - 1:
+                    ok = 1
+                else:
+                    current_clauses.pop(i)
+                    if len(current_clauses) == 0:
+                        ok = 1
+                        ok_break = 1
+                        break
+                    counter_pops += 1
+            elif non_variable in literals:
+                if index == len(variable_list) - 1 or len(literals) == 1:
+                    ok = 0
+                    ok_break = 1
+                    break
+                else:
+                    for j in range(len(literals)):
+                        if non_variable == literals[j]:
+                            continue
+                        else:
+                            new_clause += literals[j] + "V"
+                    new_clause = new_clause[:-1]
+                    current_clauses[i] = new_clause
+    if index < len(variable_list) - 1 and ok_break == -1:
+        added_node = Node(current_clauses)
+    else:
+        added_node = Node(ok)
+    current_node.set_right(added_node)
+    create_bdd_tree(current_node.right, variable_list, index + 1)
+
+
+def initialise_bdd(cnf_bdd):
+    clauses = cnf_extract_clauses(cnf_bdd)
+    root = Node(clauses)
+    number_of_variables, variables_list = get_number_of_variables(clauses)
+    create_bdd_tree(root, variables_list, 0)
+    return root
+
+
+def test_bdd(bdd_tree_node):
+    if bdd_tree_node.left is None or bdd_tree_node.right is None:
+        if bdd_tree_node.data == 1:
+            return True
+        return False
+    if test_bdd(bdd_tree_node.left) is True:
+        return True
+    if test_bdd(bdd_tree_node.right) is True:
+        return True
+    return False
 
 
 if __name__ == '__main__':
     cnf = input()
+    bdd_root = initialise_bdd(cnf)
     fnc_matrix = fnc_input(cnf)
-    for rows in fnc_matrix.values():
-        print(rows)
     counter_variables = len(fnc_matrix[0])
     bin_arr = [0] * counter_variables
     if test_fnc(fnc_matrix, counter_variables, 0, bin_arr) is True:
+        print("YES")
+    else:
+        print("NO")
+    if test_bdd(bdd_root) is True:
         print("YES")
     else:
         print("NO")
